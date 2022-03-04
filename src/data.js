54ae4dfb386c540e373d5cb26e6ae74698b3f12d
Fe-Monteiro <fe.199nanda@gmail.com>
2022-03-04 13:33:01 -0300

    Funções de filtro e porcentagem funcionando.

--- src/data.js
+++ src/data.js
@@ -1,9 +1,71 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+//Busca pelo nome digitado
+export const searchName = (data, name) => {
+  const nameSearched = data.filter((person) => person.name.toLowerCase().includes(name.toLowerCase()));
+  return nameSearched;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+// Busca por species
+export const filterSpecies = (data, species) => {
+  const resultSpecies = data.filter((person) => person.species.toLowerCase() === species.toLowerCase());
+  return resultSpecies;
 };
+
+// Busca por genêro
+export const filterGender = (data, gender) => {
+  const resultGender = data.filter((person) => person.gender.toLowerCase() === gender.toLowerCase());
+  return resultGender;
+};
+
+// Busca por status
+export const filterStatus = (data, status) => {
+  const resultStatus = data.filter((person) => person.status.toLowerCase() === status.toLowerCase());
+  return resultStatus;
+};
+
+// Busca por ordem alfabetica
+export const alphabeticalOrder = (data, order) => {
+  if (order === "AZ") {
+    return data.sort((a, z) => a.name > z.name ? 1 : -1)
+  } else {
+    return data.sort((a, z) => a.name > z.name ? -1 : 1)
+  }
+};
+
+export const calculatePercentage = (total, fractionItem) => {
+  const percentage = Math.round((fractionItem * 100) / total);
+  return percentage;
+};
+
+
+
+
+
+
+
+
+//  export const filterData = (data, filterSpecies, filterGender, filterStatus) => {
+//   const allFiltersAreChosen = filterSpecies !== "" && filterGender !== "" && filterStatus !== "";
+//   const speciesFilterIsNotEmpty = filterSpecies !== "";
+//   const genderFilterIsNotEmpty = filterGender !== "";
+//   const statusFilterIsNotEmpty = filterStatus !== "";
+//   let filterResults = "";
+
+//   if (allFiltersAreChosen) {
+//     filterResults = data.filter(results => results.species === filterSpecies && results.gender === filterGender && results.status === filterStatus);
+//     return filterResults;
+//   }
+//   if (speciesFilterIsNotEmpty) {
+//     filterResults = data.filter(results => results.species === filterSpecies);
+//     return filterResults;
+//   }
+//   if (genderFilterIsNotEmpty) {
+//     filterResults = data.filter(results => results.gender === filterGender);
+//     return filterResults;
+//   }
+//   if (statusFilterIsNotEmpty) {
+//     filterResults = data.filter(results => results.status === filterStatus);
+//     return filterResults;
+//   }else{
+//     return false
+//   }
+// };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,106 @@
-import { example } from './data.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/lol/lol.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/ghibli/ghibli.js';
-// import data from './data/athletes/athletes.js';
+import { searchName, filterSpecies, filterGender, filterStatus, alphabeticalOrder, calculatePercentage } from './data.js';
 
-console.log(example, data);
+import data from './data/rickandmorty/rickandmorty.js';
+
+const personsInfo = data.results;
+const selectSpecies = document.getElementById("species-filter");
+const selectGender = document.getElementById("gender-filter");
+const selectStatus = document.getElementById("status-filter");
+const selectOrder = document.getElementById("alphabetical-order");
+const searchNamePersona = document.getElementById("character-search");
+const showFilterPercentage = document.getElementById("filter-compatible-calculation");
+const selectButtonClearFilters = document.getElementById("clear-filters");
+
+function printCards(data) {
+  document.getElementById("cards-container").innerHTML = data.map((item) =>
+    ` <section class="box">
+        <h3 class="name-person"> ${item.name}</h3>
+        <img class="image-person" src="${item.image}"/>
+        <div class="card-info">
+          <p><strong>Espécie: </strong>${item.species}</p>
+          <p><strong>Gênero: </strong>${item.gender}</p>
+          <p><strong>Status: </strong>${item.status}</p>
+          <p><strong>Origem: </strong>${item.origin.name}</p>
+          <p><strong>Localização: </strong>${item.location.name}</p>
+          <p><strong>Número de episódios em que aparece: </strong>${item.episode.length}</p>
+        </div>
+      </section>
+` ).join('');
+}
+printCards(personsInfo);
+
+function showPercentage(data) {
+  showFilterPercentage.innerHTML = `Essa categoria representa ${data}`;
+}
+
+function searchByName(event) {
+  const personaName = searchName(personsInfo, event.target.value);
+  return printCards(personaName);
+}
+searchNamePersona.addEventListener("keyup", searchByName);
+
+function showResultOfSpeciesFilter(event) {
+  const resultSpecies = filterSpecies(personsInfo, event.target.value);
+  const speciesPercentage = `${calculatePercentage(personsInfo.length, resultSpecies.length)}% dos personagens da série Rick and Morty.`;
+  showPercentage(speciesPercentage);
+  return printCards(resultSpecies);
+}
+selectSpecies.addEventListener("change", showResultOfSpeciesFilter);
+
+function showResultOfGenderFilter(event) {
+  const resultGender = filterGender(personsInfo, event.target.value);
+  const genderPercentage = `${calculatePercentage(personsInfo.length, resultGender.length)}% dos personagens da série Rick and Morty.`;
+  showPercentage(genderPercentage);
+  return printCards(resultGender);
+}
+selectGender.addEventListener("change", showResultOfGenderFilter);
+
+function showResultOfStatusFilter(event) {
+  const resultStatus = filterStatus(personsInfo, event.target.value);
+  const statusPercentage = `${calculatePercentage(personsInfo.length, resultStatus.length)}% dos personagens da série Rick and Morty.`;
+  showPercentage(statusPercentage);
+  return printCards(resultStatus);
+}
+selectStatus.addEventListener("change", showResultOfStatusFilter);
+
+function showAlphabeticalOrder(event) {
+  const order = alphabeticalOrder(personsInfo, event.target.value);
+  return printCards(order);
+}
+selectOrder.addEventListener("change", showAlphabeticalOrder);
+
+function clearFilters(event) {
+  event.preventDefault();
+  printCards(personsInfo);
+  selectSpecies.options[(selectSpecies.selectedIndex = 0)];
+  selectGender.options[(selectGender.selectedIndex = 0)];
+  selectStatus.options[(selectStatus.selectedIndex = 0)];
+  selectOrder.options[(selectOrder.selectedIndex = 0)];
+  searchNamePersona.value = "";
+  showFilterPercentage.innerHTML = "";
+}
+selectButtonClearFilters.addEventListener("click", clearFilters);
+
+
+// function printButtonSpecies(personsInfo, speciesHtml) {
+//   let filterConcat = [];
+//   const speciesType = personsInfo.map(({species}) => (species))
+
+//     speciesType.forEach(function(typeValueOf) {
+//       filterConcat = filterConcat.concat(typeValueOf);
+//     })
+
+//     const filters = [...new Set(filterConcat)]
+
+//     filters.forEach(function(newFilters){
+//       speciesHtml.insertAdjacentHTML( `<option value="${newFilters}" class="species-filter">${newFilters.charAt(0).toUpperCase() + (newFilters.slice(1, newFilters.length))}</option>`)
+//     })
+//  }
+//  printButtonSpecies(personsInfo);
+
+// function printButtonSpecies(data){
+//   let species = personsInfo.map((item) => (item.species))
+//   const uniqueSpecies = [...new Set(species)]
+
+//   uniqueSpecies.forEach(function())
+// }
